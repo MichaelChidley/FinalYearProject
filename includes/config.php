102cc994f228c7066d8082ea4a8a9346d17ce3ff
Michael Chidley <michael@michaelchidley.co.uk>
2015-03-04 16:41:26 +0000

    Added support for bootstrap + jquery
    Added support for bootstrap + jquery. Starting initial front-end
    development of site.

---
+++ includes/config.php
@@ -0,0 +1,23 @@
+<?php
+
+	//CONFIGURATION SETTINGS
+	$configArray = array();
+
+	$configArray['SITE_URL'] = "http://127.0.0.1/Uni/FinalYearProject/";
+	$configArray['API_URL'] = "http://127.0.0.1/Uni/FinalYearProject/api/";
+
+	$configArray['API_KEY'] = "CxcgOD9E9drJdnemNFbsBB8j2yNftWv+44w+e8PkmylQzsNz2RzPdxdMiE6tjCCB";
+
+
+	//WEBSITE STYLING SETTINGS: CSS, JS ETC
+	$styleArray = array();
+
+	$styleArray['MAIN_CSS'] = $configArray['SITE_URL']."main.css";
+	$styleArray['BOOTSTRAP_CSS'] = $configArray['SITE_URL']."theme/bootstrap/css/bootstrap.css";
+
+	$styleArray['MAIN_JS'] = $configArray['SITE_URL']."main.js";
+	$styleArray['BOOTSTRAP_JS'] = $configArray['SITE_URL']."theme/bootstrap/js/bootstrap.js";
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+
+	<head>
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+
+	</head>
+
+
+	<body>
+
+	</body>
+
+
+</html>
